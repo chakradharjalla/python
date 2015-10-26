51655c0d8438259451970c16b40ab25b2b631e9d
chakadharjalla <pavankumar.jalla@sjsu.edu>
2015-10-25 18:46:12 -0700

    ____

--- Using Python as a Calculator.py
+++ Using Python as a Calculator.py
@@ -1,1 +1,24 @@
 __author__ = 'pavan'
+
+
+"""program for calculator """
+"""addition, subtraction, multiplication , division"""
+
+a= 10
+b =20
+c= 40
+
+d =a+b
+e = a-b
+f = d/e
+g = f%a
+print a
+print b
+print c
+print d
+print e
+print f
+print g
+
+
+
