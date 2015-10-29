6cfbb0c09b20f5134b831ccd2183a64f23c6aeeb
chakadharjalla <pavankumar.jalla@sjsu.edu>
2015-10-29 01:03:39 -0700

    basic comparision operators

---
+++ comparision_operators.py
@@ -0,0 +1,37 @@
+__author__ = 'pavan'
+
+
+"""program for comparision operatorsr """
+
+a = raw_input("enter the value of a")
+b = raw_input("enter the value of b")
+
+if (a > b):
+    print "a is greater than b"
+else:
+    print " b is greater or might be equal to a"
+
+if (a<b):
+    print "a is less than b"
+else:
+    print"b is less or might be equal to a"
+if (a==b):
+    print "a and b are equal"
+else:
+    print" a and b are not equal"
+
+if (a!=b):
+    print "a and b are not equal"
+else:
+    print " a and b might be equal greater or less "
+
+if (a<b):
+    print"a is less than or equal to b"
+else:
+    print"b is greater than a"
+
+
+
+
+
+
