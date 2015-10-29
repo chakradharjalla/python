6b037efc84cd2fa34c62f7ba1f792e886f00d1f4
chakadharjalla <pavankumar.jalla@sjsu.edu>
2015-10-29 01:01:53 -0700

    basic arithmatic operators

---
+++ airthametic_operators.py
@@ -0,0 +1,24 @@
+__author__ = 'pavan'
+
+
+
+a= 103
+b = 3423
+c = 334234
+d=a+b
+print "d: addition of a+b ",d
+e =a-b
+print "e: subtraction of a-b",e
+f = a*b
+print "f:multiplication of a*b",f
+g = a/b
+print "g : division of a by b",g
+h = a%b
+print "h : modulo division of a" , h
+i = a**b
+print "i : a to the power of b ", i
+k = a//b
+print "k floor division between a and b",k
+
+
+
