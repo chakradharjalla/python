262e9cc648d0a61b710b87df445b6d38739e23ea
chakadharjalla <pavankumar.jalla@sjsu.edu>
2015-10-30 01:51:13 -0700

    python membership operators

---
+++ python_membership_operators.py
@@ -0,0 +1,15 @@
+__author__ = 'pavan'
+
+
+a =54453
+b = 243234
+c = [232,4,34,2323,4535,234,54453]
+if (a in c):
+    print'a is listed in c'
+else:
+    print"a is not in c"
+
+if (b not in c):
+    print'b is not listed in c'
+else:
+    print"b is in c"
